2a0a13af04c4991ca0b022e64dcc7fa245b61cbd
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-01-04 19:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstChar(text) {
   // your code here
+	for(let ch of text){
+		if(ch!==" ")return ch;
+	}
+	return "";
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:"); 
 alert(firstChar(text));
